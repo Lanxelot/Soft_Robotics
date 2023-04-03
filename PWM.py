3e6d68b4cd08a2970b65e923e6c5c74d26c7a9f3
Aj <el20al@leeds.ac.uk>
2023-04-03 16:52:31 +0100

    ADC

--- PWM.py
+++ PWM.py
@@ -1,17 +1,12 @@
-import RPi.GPIO as GPIO
-from time import sleep
+from ADCDACPi import ADCDACPi
+import time
 
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12,GPIO.OUT)
-pi = GPIO.PWM(12,1)
-pi.start(50)
-input('hhhh')
-pi.stop()
-GPIO.cleanup()
-	
-
+adcdac = ADCDACPi(1)
+adcdac.set_adc_refvoltage(3.3)
+while True:
+	print(adcdac.read_adc_voltage(1,0))
+	time.sleep(0.5)
 
 
 	
